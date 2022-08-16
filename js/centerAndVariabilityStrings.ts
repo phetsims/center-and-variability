d1ea9f7c56be681c49e2f32393708d4edca34df3
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:34 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/centerAndVariabilityStrings.ts
+++ js/centerAndVariabilityStrings.ts
@@ -5,35 +5,58 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import centerAndVariability from './centerAndVariability.js';
 
 type StringsType = {
   'center-and-variability': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'screen': {
     'median': string;
+    'medianProperty': TReadOnlyProperty<string>;
     'meanAndMedian': string;
+    'meanAndMedianProperty': TReadOnlyProperty<string>;
     'variability': string;
+    'variabilityProperty': TReadOnlyProperty<string>;
     'lab': string;
+    'labProperty': TReadOnlyProperty<string>;
   };
   'medianQuestion': string;
+  'medianQuestionProperty': TReadOnlyProperty<string>;
   'meanAndMedianQuestion': string;
+  'meanAndMedianQuestionProperty': TReadOnlyProperty<string>;
   'variabilityQuestion': string;
+  'variabilityQuestionProperty': TReadOnlyProperty<string>;
   'kickValue': string;
+  'kickValueProperty': TReadOnlyProperty<string>;
   'distanceInMeters': string;
+  'distanceInMetersProperty': TReadOnlyProperty<string>;
   'sortData': string;
+  'sortDataProperty': TReadOnlyProperty<string>;
   'meanEqualsValue': string;
+  'meanEqualsValueProperty': TReadOnlyProperty<string>;
   'medianEqualsValue': string;
+  'medianEqualsValueProperty': TReadOnlyProperty<string>;
   'plotType': string;
+  'plotTypeProperty': TReadOnlyProperty<string>;
   'dotPlot': string;
+  'dotPlotProperty': TReadOnlyProperty<string>;
   'linePlot': string;
+  'linePlotProperty': TReadOnlyProperty<string>;
   'median': string;
+  'medianProperty': TReadOnlyProperty<string>;
   'mean': string;
+  'meanProperty': TReadOnlyProperty<string>;
   'predictMean': string;
+  'predictMeanProperty': TReadOnlyProperty<string>;
   'predictMedian': string;
+  'predictMedianProperty': TReadOnlyProperty<string>;
   'valueUnknown': string;
+  'valueUnknownProperty': TReadOnlyProperty<string>;
   'youSortedTheData': string;
+  'youSortedTheDataProperty': TReadOnlyProperty<string>;
 };
 
 const centerAndVariabilityStrings = getStringModule( 'CENTER_AND_VARIABILITY' ) as StringsType;
