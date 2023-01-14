e153a81aae92bb81b86e73c37c66b5058107b31c
phet-dev <phetadmin@gmail.com>
2023-01-14 04:12:38 -0700

    update copyright dates from daily grunt work

--- js/common/view/SimulationPreferencesContentNode.ts
+++ js/common/view/SimulationPreferencesContentNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2023, University of Colorado Boulder
 
 /**
  * General simulation controls that globally change the presentation or behavior of the simulation.
