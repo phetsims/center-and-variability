980c194949b0d6f4e5cbc2df584e55b67d4daf08
Sam Reid <reids@colorado.edu>
2023-04-23 14:49:19 -0600

    Add t-shirt icons, see https://github.com/phetsims/center-and-variability/issues/149

--- js/variability/view/DistributionRadioButtonGroup.ts
+++ js/variability/view/DistributionRadioButtonGroup.ts
@@ -5,7 +5,9 @@ import DistributionType from '../model/DistributionType.js';
 import centerAndVariability from '../../centerAndVariability.js';
 import optionize, { EmptySelfOptions } from '../../../../phet-core/js/optionize.js';
 import Property from '../../../../axon/js/Property.js';
-import { Text } from '../../../../scenery/js/imports.js';
+import { Path, Text, Node } from '../../../../scenery/js/imports.js';
+import tshirtSolidShape from '../../../../sherpa/js/fontawesome-5/tshirtSolidShape.js';
+import Tandem from '../../../../tandem/js/Tandem.js';
 
 type SelfOptions = EmptySelfOptions;
 type DistributionRadioButtonGroupOptions = SelfOptions & RectangularRadioButtonGroupOptions;
@@ -13,19 +15,45 @@ type DistributionRadioButtonGroupOptions = SelfOptions & RectangularRadioButtonG
 export default class DistributionRadioButtonGroup extends RectangularRadioButtonGroup<DistributionType> {
 
   public constructor( property: Property<DistributionType>, providedOptions: DistributionRadioButtonGroupOptions ) {
-    const options = optionize<DistributionRadioButtonGroupOptions, SelfOptions, RectangularRadioButtonGroupOptions>()( {}, providedOptions );
+    const options = optionize<DistributionRadioButtonGroupOptions, SelfOptions, RectangularRadioButtonGroupOptions>()( {
+      radioButtonOptions: {
+        baseColor: 'white'
+      }
+    }, providedOptions );
+
+    const createTShirtIcon = ( tandem: Tandem, label: string, fill: string ) => {
+
+      const path = new Path( tshirtSolidShape, {
+        fill: fill,
+        stroke: 'black',
+        lineWidth: 12,
+        maxWidth: 35
+      } );
+
+      const text = new Text( label, {
+        fontSize: 16,
+        fontWeight: 'bold',
+        fill: 'black',
+        center: path.center
+      } );
+      return new Node( {
+        children: [ path, text ]
+      } );
+    };
     super( property, [ {
       value: DistributionType.SYMMETRIC_SMALL,
-      createNode: tandem => new Text( 'Symmetric, small' )
+      createNode: tandem => createTShirtIcon( tandem, '1', '#ec5f3a' )
     }, {
       value: DistributionType.SYMMETRIC_LARGE,
-      createNode: tandem => new Text( 'Symmetric, large' )
+      createNode: tandem => createTShirtIcon( tandem, '2', '#5bc760' )
     }, {
       value: DistributionType.SKEWED,
-      createNode: tandem => new Text( 'Skewed' )
+      createNode: tandem => createTShirtIcon( tandem, '3', '#fdf454' )
     }, {
       value: DistributionType.BIMODAL,
-      createNode: tandem => new Text( 'Bimodal' )
+
+      // TODO: I sampled this color from the design doc, but it is so dark you cannot see the number against it
+      createNode: tandem => createTShirtIcon( tandem, '4', '#5324f5' )
     } ], options );
   }
 }
