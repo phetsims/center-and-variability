fc50ce8ce53c83c16a62f1bf9509ca31a5330d9a
Sam Reid <reids@colorado.edu>
2023-04-23 20:44:38 -0600

    Add text readout for range, see https://github.com/phetsims/center-and-variability/issues/149

--- js/variability/view/VariabilityPlotNode.ts
+++ js/variability/view/VariabilityPlotNode.ts
@@ -9,11 +9,14 @@
  */
 
 import centerAndVariability from '../../centerAndVariability.js';
-import { NodeOptions, Rectangle } from '../../../../scenery/js/imports.js';
+import { NodeOptions, Rectangle, Text } from '../../../../scenery/js/imports.js';
 import PickRequired from '../../../../phet-core/js/types/PickRequired.js';
 import CAVPlotNode from '../../common/view/CAVPlotNode.js';
 import VariabilityModel from '../model/VariabilityModel.js';
 import VariabilityType from '../model/VariabilityType.js';
+import MedianBarNode from '../../common/view/MedianBarNode.js';
+import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
+import Utils from '../../../../dot/js/Utils.js';
 
 export type CAVPlotOptions = NodeOptions & PickRequired<NodeOptions, 'tandem'>;
 
@@ -22,11 +25,24 @@ export default class VariabilityPlotNode extends CAVPlotNode {
   public constructor( model: VariabilityModel, numberLineWidth: number, providedOptions?: CAVPlotOptions ) {
     super( model, numberLineWidth, providedOptions );
 
+    // TODO: Combine into a single node
+    // TODO: Rename if we continue to use it here like this
+    const rangeTextReadout = new Text( '', {
+      font: new PhetFont( 13 )
+    } );
+    const rangeBars = new MedianBarNode( {
+      notchDirection: 'down',
+      barStyle: 'continuous',
+      stroke: 'black',
+      lineWidth: 1
+    } );
     const rangeRectangle = new Rectangle( 0, 50, 100, 70, {
       fill: '#c3fdb9',
       stroke: 'lightGray'
     } );
+    this.addChild( rangeBars );
     this.addChild( rangeRectangle );
+    this.addChild( rangeTextReadout );
 
     const modelViewTransform = this.modelViewTransform;
 
@@ -49,9 +65,19 @@ export default class VariabilityPlotNode extends CAVPlotNode {
         rangeRectangle.left = left;
         rangeRectangle.bottom = floor;
         rangeRectangle.visible = true;
+        rangeBars.setMedianBarShape( rangeRectangle.top - MedianBarNode.NOTCH_HEIGHT - 2, rangeRectangle.left, 0, rangeRectangle.right, false );
+
+        // TODO: The design doc called for a decimal point, but I think we should omit it because it is always an integer
+        rangeTextReadout.string = Utils.toFixed( rightmostDot.valueProperty.value! - leftmostDot.valueProperty.value!, 1 );
+        rangeTextReadout.centerX = rangeRectangle.centerX;
+        rangeTextReadout.bottom = rangeBars.top - 5;
+        rangeBars.visible = true;
+        rangeTextReadout.visible = true;
       }
       else {
         rangeRectangle.visible = false;
+        rangeBars.visible = false;
+        rangeTextReadout.visible = false;
       }
     };
     model.objectChangedEmitter.addListener( updateRangeNode );
