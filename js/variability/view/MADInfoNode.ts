7ccdf915ac5705f9a9f6659dcc5022b1759582da
Sam Reid <reids@colorado.edu>
2023-04-24 21:44:39 -0600

    Add MADInfoNode calculation, see https://github.com/phetsims/center-and-variability/issues/149

--- js/variability/view/MADInfoNode.ts
+++ js/variability/view/MADInfoNode.ts
@@ -1,7 +1,7 @@
 // Copyright 2023, University of Colorado Boulder
 
 import DerivedProperty from '../../../../axon/js/DerivedProperty.js';
-import { HBox, Text, VBox, VStrut } from '../../../../scenery/js/imports.js';
+import { HBox, HSeparator, Text, VBox, VStrut } from '../../../../scenery/js/imports.js';
 import CenterAndVariabilityStrings from '../../CenterAndVariabilityStrings.js';
 import PatternStringProperty from '../../../../axon/js/PatternStringProperty.js';
 import VariabilityPlotNode from './VariabilityPlotNode.js';
@@ -17,22 +17,54 @@ export default class MADInfoNode extends VBox {
 
     const hasEnoughDataProperty = new DerivedProperty( [ model.numberOfDataPointsProperty ], numberOfDataPoints => numberOfDataPoints >= 1 );
 
+    const numeratorText = new Text( '', { fontSize: 16 } );
+    const denominatorText = new Text( '', { fontSize: 16 } );
+
+    const resultNumeratorText = new Text( '', { fontSize: 16 } );
+    const resultDenominatorText = new Text( '', { fontSize: 16 } );
+
+    model.objectChangedEmitter.addListener( () => {
+      const values = model.getSortedLandedObjects().map( landedObject => landedObject.valueProperty.value! );
+      const mads = values.map( value => Math.abs( value - model.meanValueProperty.value! ) );
+      const madStrings = mads.map( mad => Utils.toFixed( mad, 1 ) );
+      numeratorText.string = madStrings.join( ' + ' );
+      denominatorText.string = values.length.toString();
+
+      // TODO-design: If the roundings don't match up, should we keep adding decimal points until the roundings match?
+      const sum = _.reduce( mads, ( sum, mad ) => sum + mad, 0 );
+      resultNumeratorText.string = Utils.toFixed( sum, 1 );
+      resultDenominatorText.string = values.length.toString();
+    } );
+
+    const calculationFraction = new VBox( {
+      children: [ numeratorText, new HSeparator(), denominatorText ],
+      align: 'center'
+    } );
+
+    const resultFraction = new VBox( {
+      children: [ resultNumeratorText, new HSeparator(), resultDenominatorText ],
+      align: 'center'
+    } );
+
     super( {
       align: 'left',
-      spacing: 5,
+      spacing: 6,
       children: [
         new Text( CenterAndVariabilityStrings.meanAbsoluteDeviationMADStringProperty, { fontSize: 25 } ),
         new VStrut( 10 ),
         new Text( CenterAndVariabilityStrings.madDescriptionStringProperty, { fontSize: 18 } ),
         new HBox( {
-          children: [ new Text( CenterAndVariabilityStrings.madCalculationPatternStringProperty, { fontSize: 18 } ) ]
-        } ),
+          spacing: 10,
+          children: [
+            new Text( CenterAndVariabilityStrings.madCalculationPatternStringProperty, { fontSize: 18 } ),
+            calculationFraction,
 
-        // TODO: String key name
-        // new Text( new PatternStringProperty( CenterAndVariabilityStrings.rangeCalculationPatternStringProperty, {
-        //   max: model.maxValueProperty,
-        //   min: model.minValueProperty
-        // } ), { fontSize: 18, visibleProperty: hasEnoughDataProperty } ),
+            // TODO: We don't typically i18n math, and even if we did, we don't have a placeholder language
+            // to put nodes in the placeholders. So probably this is good. But should be translate the equals sign anyways?
+            new Text( '=', { fontSize: 18 } ),
+            resultFraction
+          ]
+        } ),
 
         // TODO-design: I changed the wording slightly from the design doc
         new Text( new PatternStringProperty( CenterAndVariabilityStrings.madCalculationResultPatternStringProperty, {
