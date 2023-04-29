73a8be076001639ffe318c98ecb7e9fe0764552f
phet-dev <phetadmin@gmail.com>
2023-04-29 03:34:00 -0600

    update copyright dates from daily grunt work

--- js/common/view/DataPointNode.ts
+++ js/common/view/DataPointNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-2023, University of Colorado Boulder
+// Copyright 2023, University of Colorado Boulder
 
 import CAVObjectNode, { CAVObjectNodeOptions } from './CAVObjectNode.js';
 import centerAndVariability from '../../centerAndVariability.js';
