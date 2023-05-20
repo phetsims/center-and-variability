ba7f6d5700ba3ddc4e83ed7daf499aaaefa93a65
Sam Reid <reids@colorado.edu>
2023-05-20 05:30:01 -0600

    Remove completed TODO, see https://github.com/phetsims/center-and-variability/issues/180

--- js/variability/view/MADInfoNode.ts
+++ js/variability/view/MADInfoNode.ts
@@ -18,7 +18,6 @@ import BooleanProperty from '../../../../axon/js/BooleanProperty.js';
 export default class MADInfoNode extends VBox {
   public constructor( model: VariabilityModel, sceneModel: VariabilitySceneModel, options: PickRequired<PhetioObject, 'tandem'> ) {
 
-    // TODO: The design calls for a depiction of the mean location.  This is not yet implemented. See https://github.com/phetsims/center-and-variability/issues/180
     const hasEnoughDataProperty = new DerivedProperty( [ sceneModel.numberOfDataPointsProperty ], numberOfDataPoints => numberOfDataPoints >= 1 );
 
     const numeratorText = new Text( '', { fontSize: 16, maxWidth: 550 } );
