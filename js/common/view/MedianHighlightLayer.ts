23d4c90a67fd473971be347b3e69a55e3b271899
phet-dev <phetadmin@gmail.com>
2023-05-25 03:40:22 -0600

    update copyright dates from daily grunt work

--- js/common/view/MedianHighlightLayer.ts
+++ js/common/view/MedianHighlightLayer.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-2023, University of Colorado Boulder
+// Copyright 2023, University of Colorado Boulder
 
 import { Circle, Node, NodeOptions } from '../../../../scenery/js/imports.js';
 import centerAndVariability from '../../centerAndVariability.js';
