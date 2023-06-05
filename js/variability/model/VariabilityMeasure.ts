40a8142e1b9fe3d337ce6a5de2f19f6c2370b4ee
Marla Schulz <marlaa.schulz@gmail.com>
2023-06-05 10:35:18 -0600

    Correct spelling, see: https://github.com/phetsims/center-and-variability/issues/241

--- js/variability/model/VariabilityMeasure.ts
+++ js/variability/model/VariabilityMeasure.ts
@@ -5,16 +5,16 @@ import EnumerationValue from '../../../../phet-core/js/EnumerationValue.js';
 import centerAndVariability from '../../centerAndVariability.js';
 
 /**
- * VariabiltyMeasure is used to identify which kind of variability the user has selected to explore.
+ * VariabilityMeasure is used to identify which kind of variability the user has selected to explore.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  */
 
-export default class VariabiltyMeasure extends EnumerationValue {
-  public static readonly RANGE = new VariabiltyMeasure();
-  public static readonly IQR = new VariabiltyMeasure();
-  public static readonly MAD = new VariabiltyMeasure();
-  private static readonly enumeration = new Enumeration( VariabiltyMeasure );
+export default class VariabilityMeasure extends EnumerationValue {
+  public static readonly RANGE = new VariabilityMeasure();
+  public static readonly IQR = new VariabilityMeasure();
+  public static readonly MAD = new VariabilityMeasure();
+  private static readonly enumeration = new Enumeration( VariabilityMeasure );
 }
 
-centerAndVariability.register( 'VariabiltyMeasure', VariabiltyMeasure );
+centerAndVariability.register( 'VariabilityMeasure', VariabilityMeasure );
