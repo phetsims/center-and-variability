6509dbe5e8b0177e8677ab4882aee9350227577b
phet-dev <phetadmin@gmail.com>
2023-06-14 03:27:52 -0600

    update copyright dates from daily grunt work

--- js/soccer-common/view/SoccerPlayerGroupNumbered.ts
+++ js/soccer-common/view/SoccerPlayerGroupNumbered.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-2023, University of Colorado Boulder
+// Copyright 2023, University of Colorado Boulder
 
 /**
  * Loads a list of 4 different soccer player images, where the players are numbered.
