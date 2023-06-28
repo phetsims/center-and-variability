57ef0107cb90a9b3e6122c04b64c5d9595371250
Sam Reid <reids@colorado.edu>
2023-06-28 07:45:08 -0600

    Minor updates: Add comments, reuse font, omit dimensions for Rectangle constructor, do not center titleText on creation, see https://github.com/phetsims/center-and-variability/issues/234

--- js/common/view/AccordionBoxTitleNode.ts
+++ js/common/view/AccordionBoxTitleNode.ts
@@ -1,37 +1,42 @@
 // Copyright 2023, University of Colorado Boulder
 
 /**
- * Shows the title in the accordion box.
+ * Shows the title in the accordion box. Has a semi-transparent background in case some data goes behind it.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Matt Blackman (PhET Interactive Simulations)
+ * @author Marla Schulz (PhET Interactive Simulations)
  */
 
 import centerAndVariability from '../../centerAndVariability.js';
 import { ManualConstraint, Node, Rectangle, TColor, Text } from '../../../../scenery/js/imports.js';
 import TReadOnlyProperty from '../../../../axon/js/TReadOnlyProperty.js';
-import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
+import CAVConstants from '../CAVConstants.js';
 
 export default class AccordionBoxTitleNode extends Node {
   public constructor( stringProperty: TReadOnlyProperty<string>, backgroundFill: TColor ) {
 
     const titleText = new Text( stringProperty, {
-      font: new PhetFont( 16 ),
+      font: CAVConstants.MAIN_FONT,
+
+      // About 1/3 of the way across the accordion box, so it doesn't overlap with too much data
       maxWidth: 300
     } );
 
-    const titleBackground = new Rectangle( 0, 0, 80, 20, { sizable: true, layoutOptions: { grow: 1, stretch: true }, fill: backgroundFill, opacity: 0.5 } );
-    titleText.center = titleBackground.center;
+    // Will be sized in the manual constraint
+    const titleBackground = new Rectangle( 0, 0, 0, 0, {
+      sizable: true,
+      layoutOptions: { grow: 1, stretch: true },
+      fill: backgroundFill,
+      opacity: 0.5
+    } );
 
     super( { children: [ titleBackground, titleText ], layoutOptions: { grow: 1, stretch: true } } );
-
 
     ManualConstraint.create( this, [ titleText ], titleTextProxy => {
       titleBackground.rectBounds = titleTextProxy.bounds;
       titleTextProxy.center = titleBackground.center;
     } );
-
-
   }
 }
 
