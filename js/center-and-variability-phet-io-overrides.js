adbd1f95baa76173af949216910151897e9321e6
Sam Reid <reids@colorado.edu>
2023-08-11 07:58:49 -0600

    Move TODOS to issue, see https://github.com/phetsims/center-and-variability/issues/445

--- js/center-and-variability-phet-io-overrides.js
+++ js/center-and-variability-phet-io-overrides.js
@@ -1,14 +1,10 @@
 /* eslint-disable */
 window.phet.preloads.phetio.phetioElementsOverrides =
   {
-
-    // TODO: https://github.com/phetsims/center-and-variability/issues/445 How do I do this one? Should it be that way in common code?
-    'centerAndVariability.meanAndMedianScreen.activeProperty': {
-      phetioFeatured: true
+    "centerAndVariability.meanAndMedianScreen.activeProperty": {
+      "phetioFeatured": true
     },
-
-    // TODO: https://github.com/phetsims/center-and-variability/issues/445 How do I do this one? Should it be that way in common code?
-    'centerAndVariability.variabilityScreen.view.visibleProperty': {
-      phetioFeatured: true
+    "centerAndVariability.variabilityScreen.view.visibleProperty": {
+      "phetioFeatured": true
     }
   };
