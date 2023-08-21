87dc3b61efe764a72d6f606fd9c253c973039e1d
pixelzoom <cmalley@pixelzoom.com>
2023-08-21 12:36:51 -0400

    https://github.com/phetsims/center-and-variability/issues/479

--- js/common/CAVQueryParameters.ts
+++ js/common/CAVQueryParameters.ts
@@ -11,22 +11,29 @@ import centerAndVariability from '../centerAndVariability.js';
 
 const CAVQueryParameters = QueryStringMachine.getAll( {
 
+  //REVIEW document
   maxKicks: {
     type: 'number',
     defaultValue: 15,
     validValues: [ 5, 10, 15, 20, 25, 30 ],
     public: true
   },
+
+  //REVIEW document
   plotType: {
     type: 'string',
     validValues: [ 'dotPlot', 'linePlot' ],
     defaultValue: 'linePlot',
     public: true
   },
+
+  //REVIEW document
   showOutliers: {
     type: 'flag',
     public: true
   },
+
+  //REVIEW document
   cardMovementSoundPlaybackRate: {
     type: 'number',
     defaultValue: 1.5
