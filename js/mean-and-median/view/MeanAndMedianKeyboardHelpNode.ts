69bd027696d04d7ba8714335d54f9c1f9404ebd8
Sam Reid <reids@colorado.edu>
2023-08-24 13:14:50 -0600

    Add keyboard help section, see https://github.com/phetsims/center-and-variability/issues/316

--- js/mean-and-median/view/MeanAndMedianKeyboardHelpNode.ts
+++ js/mean-and-median/view/MeanAndMedianKeyboardHelpNode.ts
@@ -10,12 +10,16 @@ import centerAndVariability from '../../centerAndVariability.js';
 import CAVKeyboardHelpNode from '../../common/view/CAVKeyboardHelpNode.js';
 import CAVKeyboardHelpSectionGrabOrRelease from '../../common/view/CAVKeyboardHelpSectionGrabOrRelease.js';
 import CAVKeyboardHelpSectionMoveGrabbedBallAndOrCard from '../../common/view/CAVKeyboardHelpSectionMoveGrabbedBallAndOrCard.js';
+import MeanAndMedianKeyboardHelpPredictSection from './MeanAndMedianKeyboardHelpPredictSection.js';
 
 export default class MeanAndMedianKeyboardHelpNode extends CAVKeyboardHelpNode {
 
   public constructor() {
-    const shapeShortcutsHelpSection = new CAVKeyboardHelpSectionMoveGrabbedBallAndOrCard( 'Move Grabbed Ball', 'Move grabbed ball' );
-    super( [ new CAVKeyboardHelpSectionGrabOrRelease( 'Grab or Release Ball' ), shapeShortcutsHelpSection ] );
+    super( [
+      new CAVKeyboardHelpSectionGrabOrRelease( 'Grab or Release Ball' ),
+      new CAVKeyboardHelpSectionMoveGrabbedBallAndOrCard( 'Move Grabbed Ball', 'Move grabbed ball' ),
+      new MeanAndMedianKeyboardHelpPredictSection()
+    ] );
   }
 }
 
---
+++ js/mean-and-median/view/MeanAndMedianKeyboardHelpPredictSection.ts
@@ -0,0 +1,36 @@
+// Copyright 2023, University of Colorado Boulder
+
+/**
+ * Help content for the KeyboardHelpDialog for moving the Predict Median.
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ */
+
+import KeyboardHelpIconFactory from '../../../../scenery-phet/js/keyboard/help/KeyboardHelpIconFactory.js';
+import KeyboardHelpSection from '../../../../scenery-phet/js/keyboard/help/KeyboardHelpSection.js';
+import KeyboardHelpSectionRow from '../../../../scenery-phet/js/keyboard/help/KeyboardHelpSectionRow.js';
+import centerAndVariability from '../../centerAndVariability.js';
+import TextKeyNode from '../../../../scenery-phet/js/keyboard/TextKeyNode.js';
+
+export default class MeanAndMedianKeyboardHelpPredictSection extends KeyboardHelpSection {
+  public constructor() {
+    super( 'Predict Median or Mean', [
+      KeyboardHelpSectionRow.labelWithIcon( 'Move prediction pointer',
+        KeyboardHelpIconFactory.leftRightArrowKeysRowIcon() ),
+      KeyboardHelpSectionRow.labelWithIcon(
+        'Move prediction pointer in larger steps',
+        KeyboardHelpIconFactory.pageUpPageDownRowIcon(), { labelOptions: { lineWrap: 200 } }
+      ), KeyboardHelpSectionRow.labelWithIcon(
+        'Move prediction pointer in smaller steps',
+        KeyboardHelpIconFactory.shiftPlusIcon( KeyboardHelpIconFactory.leftRightArrowKeysRowIcon() ), { labelOptions: { lineWrap: 200 } }
+      ), KeyboardHelpSectionRow.labelWithIcon(
+        'Jump to start of number line',
+        TextKeyNode.home(), { labelOptions: { lineWrap: 200 } }
+      ), KeyboardHelpSectionRow.labelWithIcon(
+        'Jump to end of number line',
+        TextKeyNode.end(), { labelOptions: { lineWrap: 200 } }
+      ) ] );
+  }
+}
+
+centerAndVariability.register( 'MeanAndMedianKeyboardHelpPredictSection', MeanAndMedianKeyboardHelpPredictSection );
