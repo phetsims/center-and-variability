6a0dca5e3260cde2dc68fb1b403c5359dd5cf6f0
Marla Schulz <marlaa.schulz@gmail.com>
2023-09-19 14:09:35 -0600

    Revert "Improve types, see https://github.com/phetsims/center-and-variability/issues/495"
    This reverts commit caeac6175cb10d3e720f4a123657ad80a9853336.

--- js/variability/view/RangeNode.ts
+++ js/variability/view/RangeNode.ts
@@ -18,6 +18,7 @@ import CAVColors from '../../common/CAVColors.js';
 import VariabilitySceneModel from '../model/VariabilitySceneModel.js';
 import optionize from '../../../../phet-core/js/optionize.js';
 import NumberLineNode from '../../../../soccer-common/js/view/NumberLineNode.js';
+import SoccerBall from '../../../../soccer-common/js/model/SoccerBall.js';
 import IntervalBarNode from '../../common/view/IntervalBarNode.js';
 import TProperty from '../../../../axon/js/TProperty.js';
 import NeedAtLeastNKicksText from '../../common/view/NeedAtLeastNKicksText.js';
@@ -65,12 +66,12 @@ export default class RangeNode extends CAVPlotNode {
 
       const sortedDots = sceneModel.getSortedLandedObjects();
 
-      const leftmostDot = sortedDots[ 0 ];
-      const rightmostDot = sortedDots[ sortedDots.length - 1 ];
+      const leftmostDot = sortedDots[ 0 ] as SoccerBall | undefined;
+      const rightmostDot = sortedDots[ sortedDots.length - 1 ] as SoccerBall | undefined;
 
       // Only redraw the shape if the feature is selected and the data is sorted, and there is at least one card
-      const hasNonZeroRange = leftmostDot &&
-                              rightmostDot &&
+      const hasNonZeroRange = !!leftmostDot &&
+                              !!rightmostDot &&
                               leftmostDot.valueProperty.value !== rightmostDot.valueProperty.value;
       if ( hasNonZeroRange ) {
         const left = this.modelViewTransform.modelToViewX( leftmostDot.valueProperty.value! );
