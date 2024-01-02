4c1e488cc54938b74e563d7b5628cda9e70a581f
phet-dev <phetadmin@gmail.com>
2024-01-02 03:12:28 -0700

    update copyright dates from daily grunt work

--- js/median/model/InteractiveCardContainerModel.ts
+++ js/median/model/InteractiveCardContainerModel.ts
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2023-2024, University of Colorado Boulder
 
 /**
  * InteractiveCardContainerModel manages the interactive card container in a simulation. It supports a variety
