bbbfef5e644e67be1aa221ec977e872243bbe3be
phet-dev <phetadmin@gmail.com>
2024-03-28 03:13:55 -0600

    update copyright dates from daily grunt work

--- js/mean-and-median/MeanAndMedianScreen.ts
+++ js/mean-and-median/MeanAndMedianScreen.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-2023, University of Colorado Boulder
+// Copyright 2022-2024, University of Colorado Boulder
 
 /**
  * The MeanAndMedianScreen represents the entire Mean & Median screen in the simulation.
