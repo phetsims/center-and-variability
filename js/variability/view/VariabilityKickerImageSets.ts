04f93d7ccc34950de638ff94e677750e26377f74
phet-dev <phetadmin@gmail.com>
2024-03-29 03:13:32 -0600

    update copyright dates from daily grunt work

--- js/variability/view/VariabilityKickerImageSets.ts
+++ js/variability/view/VariabilityKickerImageSets.ts
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2024, University of Colorado Boulder
 /**
  * The VariabilityKickerImageSets contains the image properties for the kickers with numbered jerseys in the
  * Variability screen.
