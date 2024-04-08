d8de17ccead3859b94a1280225ad211e1df581e0
Luisav1 <luisa.vargassuarez@ucalgary.ca>
2024-04-08 15:37:15 -0600

    Create ts image files for all Asia, Oceania, and Latin America images. Update CaV and soccer-common image files. See https://github.com/phetsims/soccer-common/issues/10.

--- js/CenterAndVariabilityImages.ts
+++ js/CenterAndVariabilityImages.ts
@@ -21,6 +21,36 @@ import africaVariabilityKicker04Standing_svg from '../../center-and-variability/
 import africaModestVariabilityKicker02Kicking_svg from '../../center-and-variability/images/africaModest/africaModestVariabilityKicker02Kicking_svg.js';
 import africaModestVariabilityKicker02PoisedToKick_svg from '../../center-and-variability/images/africaModest/africaModestVariabilityKicker02PoisedToKick_svg.js';
 import africaModestVariabilityKicker02Standing_svg from '../../center-and-variability/images/africaModest/africaModestVariabilityKicker02Standing_svg.js';
+import asiaVariabilityKicker01Kicking_svg from '../../center-and-variability/images/asia/asiaVariabilityKicker01Kicking_svg.js';
+import asiaVariabilityKicker01PoisedToKick_svg from '../../center-and-variability/images/asia/asiaVariabilityKicker01PoisedToKick_svg.js';
+import asiaVariabilityKicker01Standing_svg from '../../center-and-variability/images/asia/asiaVariabilityKicker01Standing_svg.js';
+import asiaVariabilityKicker02Kicking_svg from '../../center-and-variability/images/asia/asiaVariabilityKicker02Kicking_svg.js';
+import asiaVariabilityKicker02PoisedToKick_svg from '../../center-and-variability/images/asia/asiaVariabilityKicker02PoisedToKick_svg.js';
+import asiaVariabilityKicker02Standing_svg from '../../center-and-variability/images/asia/asiaVariabilityKicker02Standing_svg.js';
+import asiaVariabilityKicker03Kicking_svg from '../../center-and-variability/images/asia/asiaVariabilityKicker03Kicking_svg.js';
+import asiaVariabilityKicker03PoisedToKick_svg from '../../center-and-variability/images/asia/asiaVariabilityKicker03PoisedToKick_svg.js';
+import asiaVariabilityKicker03Standing_svg from '../../center-and-variability/images/asia/asiaVariabilityKicker03Standing_svg.js';
+import asiaVariabilityKicker04Kicking_svg from '../../center-and-variability/images/asia/asiaVariabilityKicker04Kicking_svg.js';
+import asiaVariabilityKicker04PoisedToKick_svg from '../../center-and-variability/images/asia/asiaVariabilityKicker04PoisedToKick_svg.js';
+import asiaVariabilityKicker04Standing_svg from '../../center-and-variability/images/asia/asiaVariabilityKicker04Standing_svg.js';
+import latinAmericaVariabilityKicker01Kicking_svg from '../../center-and-variability/images/latinAmerica/latinAmericaVariabilityKicker01Kicking_svg.js';
+import latinAmericaVariabilityKicker01PoisedToKick_svg from '../../center-and-variability/images/latinAmerica/latinAmericaVariabilityKicker01PoisedToKick_svg.js';
+import latinAmericaVariabilityKicker01Standing_svg from '../../center-and-variability/images/latinAmerica/latinAmericaVariabilityKicker01Standing_svg.js';
+import latinAmericaVariabilityKicker02Kicking_svg from '../../center-and-variability/images/latinAmerica/latinAmericaVariabilityKicker02Kicking_svg.js';
+import latinAmericaVariabilityKicker02PoisedToKick_svg from '../../center-and-variability/images/latinAmerica/latinAmericaVariabilityKicker02PoisedToKick_svg.js';
+import latinAmericaVariabilityKicker02Standing_svg from '../../center-and-variability/images/latinAmerica/latinAmericaVariabilityKicker02Standing_svg.js';
+import latinAmericaVariabilityKicker03Kicking_svg from '../../center-and-variability/images/latinAmerica/latinAmericaVariabilityKicker03Kicking_svg.js';
+import latinAmericaVariabilityKicker03PoisedToKick_svg from '../../center-and-variability/images/latinAmerica/latinAmericaVariabilityKicker03PoisedToKick_svg.js';
+import latinAmericaVariabilityKicker03Standing_svg from '../../center-and-variability/images/latinAmerica/latinAmericaVariabilityKicker03Standing_svg.js';
+import latinAmericaVariabilityKicker04Kicking_svg from '../../center-and-variability/images/latinAmerica/latinAmericaVariabilityKicker04Kicking_svg.js';
+import latinAmericaVariabilityKicker04PoisedToKick_svg from '../../center-and-variability/images/latinAmerica/latinAmericaVariabilityKicker04PoisedToKick_svg.js';
+import latinAmericaVariabilityKicker04Standing_svg from '../../center-and-variability/images/latinAmerica/latinAmericaVariabilityKicker04Standing_svg.js';
+import oceaniaVariabilityKicker01Kicking_svg from '../../center-and-variability/images/oceania/oceaniaVariabilityKicker01Kicking_svg.js';
+import oceaniaVariabilityKicker01PoisedToKick_svg from '../../center-and-variability/images/oceania/oceaniaVariabilityKicker01PoisedToKick_svg.js';
+import oceaniaVariabilityKicker01Standing_svg from '../../center-and-variability/images/oceania/oceaniaVariabilityKicker01Standing_svg.js';
+import oceaniaVariabilityKicker02Kicking_svg from '../../center-and-variability/images/oceania/oceaniaVariabilityKicker02Kicking_svg.js';
+import oceaniaVariabilityKicker02PoisedToKick_svg from '../../center-and-variability/images/oceania/oceaniaVariabilityKicker02PoisedToKick_svg.js';
+import oceaniaVariabilityKicker02Standing_svg from '../../center-and-variability/images/oceania/oceaniaVariabilityKicker02Standing_svg.js';
 import usaVariabilityKicker01Kicking_svg from '../../center-and-variability/images/usa/usaVariabilityKicker01Kicking_svg.js';
 import usaVariabilityKicker01PoisedToKick_svg from '../../center-and-variability/images/usa/usaVariabilityKicker01PoisedToKick_svg.js';
 import usaVariabilityKicker01Standing_svg from '../../center-and-variability/images/usa/usaVariabilityKicker01Standing_svg.js';
@@ -38,61 +68,97 @@ const CenterAndVariabilityImages = {
   variabilityKicker01KickingImageProperty: new LocalizedImageProperty( 'variabilityKicker01Kicking', {
     africa: africaVariabilityKicker01Kicking_svg,
     africaModest: africaVariabilityKicker01Kicking_svg,
+    asia: asiaVariabilityKicker01Kicking_svg,
+    latinAmerica: latinAmericaVariabilityKicker01Kicking_svg,
+    oceania: oceaniaVariabilityKicker01Kicking_svg,
     usa: usaVariabilityKicker01Kicking_svg
   } ),
   variabilityKicker01PoisedToKickImageProperty: new LocalizedImageProperty( 'variabilityKicker01PoisedToKick', {
     africa: africaVariabilityKicker01PoisedToKick_svg,
     africaModest: africaVariabilityKicker01PoisedToKick_svg,
+    asia: asiaVariabilityKicker01PoisedToKick_svg,
+    latinAmerica: latinAmericaVariabilityKicker01PoisedToKick_svg,
+    oceania: oceaniaVariabilityKicker01PoisedToKick_svg,
     usa: usaVariabilityKicker01PoisedToKick_svg
   } ),
   variabilityKicker01StandingImageProperty: new LocalizedImageProperty( 'variabilityKicker01Standing', {
     africa: africaVariabilityKicker01Standing_svg,
     africaModest: africaVariabilityKicker01Standing_svg,
+    asia: asiaVariabilityKicker01Standing_svg,
+    latinAmerica: latinAmericaVariabilityKicker01Standing_svg,
+    oceania: oceaniaVariabilityKicker01Standing_svg,
     usa: usaVariabilityKicker01Standing_svg
   } ),
   variabilityKicker02KickingImageProperty: new LocalizedImageProperty( 'variabilityKicker02Kicking', {
     africa: africaVariabilityKicker02Kicking_svg,
     africaModest: africaModestVariabilityKicker02Kicking_svg,
+    asia: asiaVariabilityKicker02Kicking_svg,
+    latinAmerica: latinAmericaVariabilityKicker02Kicking_svg,
+    oceania: oceaniaVariabilityKicker02Kicking_svg,
     usa: usaVariabilityKicker02Kicking_svg
   } ),
   variabilityKicker02PoisedToKickImageProperty: new LocalizedImageProperty( 'variabilityKicker02PoisedToKick', {
     africa: africaVariabilityKicker02PoisedToKick_svg,
     africaModest: africaModestVariabilityKicker02PoisedToKick_svg,
+    asia: asiaVariabilityKicker02PoisedToKick_svg,
+    latinAmerica: latinAmericaVariabilityKicker02PoisedToKick_svg,
+    oceania: oceaniaVariabilityKicker02PoisedToKick_svg,
     usa: usaVariabilityKicker02PoisedToKick_svg
   } ),
   variabilityKicker02StandingImageProperty: new LocalizedImageProperty( 'variabilityKicker02Standing', {
     africa: africaVariabilityKicker02Standing_svg,
     africaModest: africaModestVariabilityKicker02Standing_svg,
+    asia: asiaVariabilityKicker02Standing_svg,
+    latinAmerica: latinAmericaVariabilityKicker02Standing_svg,
+    oceania: oceaniaVariabilityKicker02Standing_svg,
     usa: usaVariabilityKicker02Standing_svg
   } ),
   variabilityKicker03KickingImageProperty: new LocalizedImageProperty( 'variabilityKicker03Kicking', {
     africa: africaVariabilityKicker03Kicking_svg,
     africaModest: africaVariabilityKicker03Kicking_svg,
+    asia: asiaVariabilityKicker03Kicking_svg,
+    latinAmerica: latinAmericaVariabilityKicker03Kicking_svg,
+    oceania: asiaVariabilityKicker03Kicking_svg,
     usa: usaVariabilityKicker03Kicking_svg
   } ),
   variabilityKicker03PoisedToKickImageProperty: new LocalizedImageProperty( 'variabilityKicker03PoisedToKick', {
     africa: africaVariabilityKicker03PoisedToKick_svg,
     africaModest: africaVariabilityKicker03PoisedToKick_svg,
+    asia: asiaVariabilityKicker03PoisedToKick_svg,
+    latinAmerica: latinAmericaVariabilityKicker03PoisedToKick_svg,
+    oceania: asiaVariabilityKicker03PoisedToKick_svg,
     usa: usaVariabilityKicker03PoisedToKick_svg
   } ),
   variabilityKicker03StandingImageProperty: new LocalizedImageProperty( 'variabilityKicker03Standing', {
     africa: africaVariabilityKicker03Standing_svg,
     africaModest: africaVariabilityKicker03Standing_svg,
+    asia: asiaVariabilityKicker03Standing_svg,
+    latinAmerica: latinAmericaVariabilityKicker03Standing_svg,
+    oceania: asiaVariabilityKicker03Standing_svg,
     usa: usaVariabilityKicker03Standing_svg
   } ),
   variabilityKicker04KickingImageProperty: new LocalizedImageProperty( 'variabilityKicker04Kicking', {
     africa: africaVariabilityKicker04Kicking_svg,
     africaModest: africaVariabilityKicker04Kicking_svg,
+    asia: asiaVariabilityKicker04Kicking_svg,
+    latinAmerica: latinAmericaVariabilityKicker04Kicking_svg,
+    oceania: asiaVariabilityKicker04Kicking_svg,
     usa: usaVariabilityKicker04Kicking_svg
   } ),
   variabilityKicker04PoisedToKickImageProperty: new LocalizedImageProperty( 'variabilityKicker04PoisedToKick', {
     africa: africaVariabilityKicker04PoisedToKick_svg,
     africaModest: africaVariabilityKicker04PoisedToKick_svg,
+    asia: asiaVariabilityKicker04PoisedToKick_svg,
+    latinAmerica: latinAmericaVariabilityKicker04PoisedToKick_svg,
+    oceania: asiaVariabilityKicker04PoisedToKick_svg,
     usa: usaVariabilityKicker04PoisedToKick_svg
   } ),
   variabilityKicker04StandingImageProperty: new LocalizedImageProperty( 'variabilityKicker04Standing', {
     africa: africaVariabilityKicker04Standing_svg,
     africaModest: africaVariabilityKicker04Standing_svg,
+    asia: asiaVariabilityKicker04Standing_svg,
+    latinAmerica: latinAmericaVariabilityKicker04Standing_svg,
+    oceania: asiaVariabilityKicker04Standing_svg,
     usa: usaVariabilityKicker04Standing_svg
   } )
 };
