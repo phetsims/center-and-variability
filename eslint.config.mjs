ab074883bf03394d49d83b1d05b16bb5abed4bc2
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:09:29 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for center-and-variability.
+ * ESLint configuration for center-and-variability.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
