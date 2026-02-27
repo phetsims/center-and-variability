e2fc37634ef4b278cd84065445d255438b44625b
phet-dev <phetadmin@gmail.com>
2026-02-27 04:04:12 -0700

    update copyright dates from daily grunt work

--- js/common/view/CAVScreenSummaryContent.ts
+++ js/common/view/CAVScreenSummaryContent.ts
@@ -1,4 +1,4 @@
-// Copyright 2025, University of Colorado Boulder
+// Copyright 2025-2026, University of Colorado Boulder
 /**
  * Consolidates the common aspects of screen summaries for the Center and Variability simulation.
  *
